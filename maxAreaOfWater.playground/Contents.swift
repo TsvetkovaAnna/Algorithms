edcd9589c8f4ff532241f14fb75a6a1427f38eba
TsvetkovaAnna <99097473+TsvetkovaAnna@users.noreply.github.com>
2022-10-29 23:50:53 +0600

    Update Contents.swift

--- maxAreaOfWater.playground/Contents.swift
+++ maxAreaOfWater.playground/Contents.swift
@@ -28,15 +28,3 @@ let t1 = sol.maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])
 let t2 = sol.maxArea([1, 1])
 print(t1)
 print(t2)
-
-//while r < height.count {
-//    let mainHeight = min(height[l], height[r])
-//    let length = r - l
-//    let currentArea = length * mainHeight
-//    area = currentArea > area ? currentArea : area
-//    r += 1
-//    if r == height.count {
-//        l += 1
-//        r = l + 1
-//    }
-//}
